cca9a454dad80d5992a94bed9148055be91dc6a0
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2025-02-20 22:14:15 -0500

    fixed about

--- src/sections/About.js
+++ src/sections/About.js
@@ -2,9 +2,7 @@ import React from "react";
 function About() {
   return (
       <section id="about" className="about-section">
-        <div className="heading">
           <h1>about</h1><p>i'm a junior studying cs!</p>
-        </div>
         </section>
   );
 }
