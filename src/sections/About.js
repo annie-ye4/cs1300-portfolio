00d53415e1ea37dbf09d7aad438023942f7d284f
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2025-05-03 00:39:59 -0400

    removing about

--- src/sections/About.js
+++ src/sections/About.js
@@ -1,9 +1,1 @@
-import React from "react";
-function About() {
-  return (
-      <section id="about" className="about-section">
-          <h1>about</h1><p>i'm a junior studying cs!</p>
-        </section>
-  );
-}
-export default About;
+// gi
